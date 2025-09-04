16a555b272f09a0314658e85c4e23d1651f7ee90
bensooraj <bensooraj@users.noreply.github.com>
2025-09-05 00:45:22 +0530

    add an https listener and configure the ssl certificate as well

--- iac/main.tf
+++ iac/main.tf
@@ -140,6 +140,20 @@ resource "aws_lb_listener" "http" {
   }
 }
 
+# HTTPS listener that uses an existing ACM certificate
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08" # or a stricter policy you prefer
+  certificate_arn   = var.acm_certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
 # --- AMI (Amazon Linux 2023)
 data "aws_ami" "al2023" {
   most_recent = true
